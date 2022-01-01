fd3531ea441e70501d4cc7aeec8c5c575593ff4f
brakesh <bandarupallirakesh@gmail.com>
2022-01-02 03:50:57 +0530

    New Yaml file added

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,3 +1,3 @@
 temp_id="lt-0803b1e328525c4de"
 version=2
-aws ec2 --launch-template LaunchTemplateId=${temp_id} Version=${version}|jq
+aws ec2 --launch-template LaunchTemplateId=${temp_id},Version=${version}  --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]"|jq
