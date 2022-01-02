0ea60afd36dab62fc52f389a9150709f868b423d
brakesh <bandarupallirakesh@gmail.com>
2022-01-02 14:15:52 +0530

    New Yaml file added

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,3 +1,7 @@
+if [ -z "$1" ]; then
+  echo "input missing"
+  exit
+fi
 temp_id="lt-0803b1e328525c4de"
 aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend"|jq .Reservations[].Instances[].State.Name | sed 's/"//g'|grep -E 'running|stopped' &>>/dev/null
 if [ $? -eq 0 ]; then
