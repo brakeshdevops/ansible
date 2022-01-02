4d75f13d41861009a20f11a492ceb1f68e3ba11e
brakesh <bandarupallirakesh@gmail.com>
2022-01-02 19:03:47 +0530

    New Yaml file added

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,15 +1,28 @@
-if [ -z "$1" ]; then
+COMPONENT=$1
+if [ -z "${COMPONENT}" ]; then
   echo "input missing"
   exit
 fi
 temp_id="lt-0803b1e328525c4de"
 ZONE_ID="Z09972541YT3UVO8YWPEG"
-aws ec2 describe-instances --filters "Name=tag:Name,Values=$1"|jq .Reservations[].Instances[].State.Name | sed 's/"//g'|grep -E 'running|stopped' &>>/dev/null
-if [ $? -eq 0 ]; then
-  echo "Instance is already there"
+CREATE()
+{
+  aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].State.Name | sed 's/"//g'|grep -E 'running|stopped' &>>/dev/null
+  if [ $? -eq 0 ]; then
+    echo "Instance is already there"
+  else
+    aws ec2 run-instances --launch-template LaunchTemplateId=${temp_id},Version=2 --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"|jq
+  fi
+  sleep 10
+  IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"|jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
+  sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+}
+if [ ${COMPONENT} == 'all' ]; then
+  for comp in frontend mongodb catalogue; do
+    COMPONENT=$1
+    CREATE
+    done
 else
-  aws ec2 run-instances --launch-template LaunchTemplateId=${temp_id},Version=2 --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"|jq
+    CREATE
 fi
-IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1"|jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
-sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/$1/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
