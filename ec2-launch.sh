0451a36992e963fbaebe1e5dd31a6c7026c91c43
brakesh <bandarupallirakesh@gmail.com>
2022-01-02 18:48:09 +0530

    New Yaml file added

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -7,10 +7,8 @@ ZONE_ID="Z09972541YT3UVO8YWPEG"
 aws ec2 describe-instances --filters "Name=tag:Name,Values=$1"|jq .Reservations[].Instances[].State.Name | sed 's/"//g'|grep -E 'running|stopped' &>>/dev/null
 if [ $? -eq 0 ]; then
   echo "Instance is already there"
-  exit
-
 else
-aws ec2 run-instances --launch-template LaunchTemplateId=${temp_id},Version=2 --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"|jq
+  aws ec2 run-instances --launch-template LaunchTemplateId=${temp_id},Version=2 --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"|jq
 fi
 IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1"|jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
 sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/$1/" record.json >/tmp/record.json
