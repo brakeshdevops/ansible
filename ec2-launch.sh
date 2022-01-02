063a47a583cff1e3c974a3a6ad2e83dab5b027ed
brakesh <bandarupallirakesh@gmail.com>
2022-01-02 14:17:44 +0530

    New Yaml file added

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -3,10 +3,10 @@ if [ -z "$1" ]; then
   exit
 fi
 temp_id="lt-0803b1e328525c4de"
-aws ec2 describe-instances --filters "Name=tag:Name,Values=frontend"|jq .Reservations[].Instances[].State.Name | sed 's/"//g'|grep -E 'running|stopped' &>>/dev/null
+aws ec2 describe-instances --filters "Name=tag:Name,Values=$1"|jq .Reservations[].Instances[].State.Name | sed 's/"//g'|grep -E 'running|stopped' &>>/dev/null
 if [ $? -eq 0 ]; then
   echo "Instance is already there"
   exit
 fi
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${temp_id},Version=2 --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=frontend}]" "ResourceType=instance,Tags=[{Key=Name,Value=frontend}]"|jq
+aws ec2 run-instances --launch-template LaunchTemplateId=${temp_id},Version=2 --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$1}]" "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"|jq
